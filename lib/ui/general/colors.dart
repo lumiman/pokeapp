a93742e27f87f1bd2073c9354a5a95c0d27cb5ce
Luis M. Diaz <ldiaz@redteam.com>
2023-07-03 16:36:16 -0500

    Fix Update colors

--- lib/ui/general/colors.dart
+++ lib/ui/general/colors.dart
@@ -6,4 +6,10 @@ Map<String, Color> colorsPokemon = {
   "Fire" : Color(0xffFB6C6C),
   "Water" : Color(0xff76BCFD),
   "Electric" : Color(0xffFFD86F),
+  "Normal" : Color(0xffE0E3E5),
+  "Ground" : Color(0xffCE8F43),
+  "Poison" : Color(0xffA44F4F),
+  "Fighting" : Color(0xff9EA2D6),
+  "Psychic" : Color(0xffD6D69E),
+  "Rock" : Color(0xffA1A19F),
 };
